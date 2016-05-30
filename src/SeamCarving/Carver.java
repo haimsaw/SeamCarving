ce708c80fb27309fbcf27f6d12d982e5ef3594eb
haimsaw <haimsaw@gmail.com>
2016-05-30 20:17:59 +0300

    main

--- src/SeamCarving/Carver.java
+++ src/SeamCarving/Carver.java
@@ -10,7 +10,9 @@ public class Carver {
 
     public static void main(String [ ] args){
         String  input_image_filename = args[0];
-
+        int num_of_colums = Integer.parseInt(args[1]);
+        int num_of_rows = Integer.parseInt(args[2]);
+        int energy_type = Integer.parseInt(args[3]);
 
 
 
