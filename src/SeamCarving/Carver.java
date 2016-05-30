61d4bc15c56eb23d6f2b24aa120d41b6885a38cc
haimsaw <haimsaw@gmail.com>
2016-05-30 20:15:43 +0300

    main

--- src/SeamCarving/Carver.java
+++ src/SeamCarving/Carver.java
@@ -1,8 +1,27 @@
 package SeamCarving;
+import javax.imageio.ImageIO;
+import java.awt.color.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+
 
 public class Carver {
 
     public static void main(String [ ] args){
+        String  input_image_filename = args[0];
+
+
+
+
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(args[1]));
+        } catch (IOException e) {
+            System.out.println("ERROR");
+        }
+
+        byte[][][] img = null;
 
     }
 }
