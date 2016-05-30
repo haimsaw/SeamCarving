5a8b2786464626689952d602dc2f545fdaf93c62
haimsaw <haimsaw@gmail.com>
2016-05-30 20:33:42 +0300

    main

--- src/SeamCarving/Carver.java
+++ src/SeamCarving/Carver.java
@@ -9,21 +9,20 @@ import java.io.IOException;
 public class Carver {
 
     public static void main(String [ ] args){
-        String  input_image_filename = args[0];
+        String input_image_filename = args[0];
         int num_of_colums = Integer.parseInt(args[1]);
         int num_of_rows = Integer.parseInt(args[2]);
         int energy_type = Integer.parseInt(args[3]);
+        String output_image_filename = args[4];
 
 
+        MyColor[][] img = GetImage(input_image_filename);
 
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File(args[1]));
-        } catch (IOException e) {
-            System.out.println("ERROR");
-        }
-
-        byte[][][] img = null;
 
     }
+
+
+
+    public static get_energy_map()
 }
+
---
+++ src/SeamCarving/Energizer.java
@@ -0,0 +1,18 @@
+package SeamCarving;
+
+
+public class Energizer {
+
+    public static energy1(MyColor[][] image){
+        int hight = image.length;
+        int width = image[0].length;
+
+        for (int i=0;i<width;i++){
+            for (int j=0;j<hight;j++){
+
+            }
+        }
+
+    }
+
+}
