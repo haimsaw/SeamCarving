994c5ab8233f4d013ad179035c2504ba81fb4711
nogalavi <noga.lavi1@gmail.com>
2016-05-30 20:32:48 +0300

    My Color + Services file

---
+++ src/SeamCarving/MyColor.java
@@ -0,0 +1,16 @@
+package SeamCarving;
+
+
+public class MyColor {
+    public float r;
+    public float g;
+    public float b;
+
+    public MyColor(float r, float g, float b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    
+
+}
---
+++ src/SeamCarving/Services.java
@@ -0,0 +1,12 @@
+package SeamCarving;
+
+
+import java.awt.image.BufferedImage;
+
+public class Services {
+
+    public static int[][] rgbToMatrix(BufferedImage bi) {
+        int[][]
+
+    }
+}
