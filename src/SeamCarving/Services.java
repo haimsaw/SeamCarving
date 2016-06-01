02323be264222ad8ffbc340efccf431393c49429
nogalavi <noga.lavi1@gmail.com>
2016-06-02 00:33:02 +0300

    buggy version

--- src/SeamCarving/Services.java
+++ src/SeamCarving/Services.java
@@ -1,8 +1,11 @@
 package SeamCarving;
 import javax.imageio.ImageIO;
+import java.awt.image.WritableRaster;
 import java.io.File;
 
 import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -21,8 +24,8 @@ public class Services {
             MyColor[][] result = new MyColor[w][h];
 
 
-            for (int i=0; i<w; i++) {
-                for (int j=0; j<h; j++) {
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
                     int rgb = bi.getRGB(i, j);
                     double red = (rgb >> 16) & 0xFF;
                     double green = (rgb >> 8) & 0xFF;
@@ -39,18 +42,18 @@ public class Services {
 
     }
 
-    public static List<Seem> get_best_seem_list(int num_of_seems, double[][] dynamicMap){
+    public static List<Seem> get_best_seem_list(int num_of_seems, double[][] dynamicMap) {
         List<Seem> seem_list = new LinkedList<>();
-        double[] seem_scors = Arrays.copyOf(dynamicMap[dynamicMap.length-1], dynamicMap[dynamicMap.length-1].length);
+        double[] seem_scors = Arrays.copyOf(dynamicMap[dynamicMap.length - 1], dynamicMap[dynamicMap.length - 1].length);
         Arrays.sort(seem_scors);
         double max_seem_val = seem_scors[num_of_seems - 1];
         //we need all the seems with val<=max_seem_val
 
-        for (int j=0; j<dynamicMap.length; j++){
-            if (dynamicMap[dynamicMap.length -1][j]<=max_seem_val){
+        for (int j = 0; j < dynamicMap.length; j++) {
+            if (dynamicMap[dynamicMap.length - 1][j] <= max_seem_val) {
                 seem_list.add(get_seem_starting_at(j, dynamicMap));
             }
-            if (seem_list.size() == num_of_seems){
+            if (seem_list.size() == num_of_seems) {
                 break;
             }
 
@@ -63,20 +66,19 @@ public class Services {
     private static Seem get_seem_starting_at(int seem_location, double[][] dynamicMap) {
         int height = dynamicMap[0].length;
         int seem_as_arr[] = new int[height];
-        seem_as_arr[height-1] = seem_location;
-        for (int i = height-2; i>=0; i--){
-            int center = seem_as_arr[i+1];
+        seem_as_arr[height - 1] = seem_location;
+        for (int i = height - 2; i >= 0; i--) {
+            int center = seem_as_arr[i + 1];
             double min = 1000000;
             int min_loc = -1;
-            for (int width_diff = -1; width_diff<=2;width_diff++){
+            for (int width_diff = -1; width_diff <= 2; width_diff++) {
                 try {
                     double min_cadidate = dynamicMap[i][center + width_diff];
-                    if (min_cadidate < min){
+                    if (min_cadidate < min) {
                         min = min_cadidate;
                         min_loc = center + width_diff;
                     }
-                }
-                catch (ArrayIndexOutOfBoundsException e){
+                } catch (ArrayIndexOutOfBoundsException e) {
                     continue;
                 }
 
@@ -89,7 +91,7 @@ public class Services {
 
     public static MyColor[][] removeSeem(MyColor[][] old, Seem seemObj) {
         int[] seem = seemObj.seem_as_arr;
-        MyColor[][] newMat = new MyColor[old.length][old[0].length-1];
+        MyColor[][] newMat = new MyColor[old.length][old[0].length - 1];
         for (int i = 0; i < old.length; i++) {
             boolean isRowAfterRemoval = false;
             for (int j = 0; j < old[0].length; j++) {
@@ -106,5 +108,30 @@ public class Services {
 
         }
         return newMat;
+    }
+
+    public static void saveImage(MyColor[][] m, String path) {
+//        File file = new File(path);
+//        BufferedImage bi = ImageIO.read(pa);
+
+
+        BufferedImage img = new BufferedImage(m.length, m[0].length,
+                BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                int r = (int) m[i][j].r; // red component 0...255
+                int g = (int) m[i][j].g; // green component 0...255
+                int b = (int) m[i][j].b; // blue component 0...255
+                int col = (r << 16) | (g << 8) | b;
+                img.setRGB(i, j, col);
+            }
+        }
+
+        File f = new File(path);
+        try {
+            ImageIO.write(img, "PNG", f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
