09d1fa4d38fdc3ec527e3a0fb9beb0042ed8eccc
haimsaw <haimsaw@gmail.com>
2016-06-01 22:22:49 +0300

    add seem class

---
+++ src/SeamCarving/seem.java
@@ -0,0 +1,11 @@
+package SeamCarving;
+
+
+public class Seem {
+    public int[] seem_as_arr;
+    // seem_as_arr[i] = j <-> img[i][j] is in the seem
+
+     public Seem(int[] arr){
+        this.seem_as_arr = arr;
+     }
+}
